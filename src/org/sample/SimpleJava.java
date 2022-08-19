1f6c47e895f95781e34e317ef1f5dd77c92fdd69
SURYA-THANSHIKA <suryajira170519@gmail.com>
2022-08-19 22:43:56 +0530

    my simple java

---
+++ src/org/sample/SimpleJava.java
@@ -0,0 +1,27 @@
+package org.sample;
+
+public class SimpleJava 
+
+{
+	public void welcomenote()
+	
+	{System.out.println("WELCOME JAVA");
+	
+	}
+	
+	public void user()
+	
+	{System.out.println("SURYA");
+	
+	}
+
+	public static void main(String[] args)
+	
+	{
+		SimpleJava s = new SimpleJava();
+		s.welcomenote();
+		s.user();
+		
+		
+	}
+}
